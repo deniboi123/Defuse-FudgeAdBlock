f6950d4a6ffd66ceb71f7cfa2ea54f8efdaebfc4
deniboi123 <58114191+deniboi123@users.noreply.github.com>
2022-01-09 12:39:31 +0000

    Update ez.js

--- ez.js
+++ ez.js
@@ -3,3 +3,7 @@ var script = document.createElement('script');
 script.innerHTML = "function adBlockDetected() {}"
 document.head.appendChild(script);
 
+var script = document.createElement('script');
+script.innerHTML = "adBlockUnDetected()"
+document.head.appendChild(script);
+
